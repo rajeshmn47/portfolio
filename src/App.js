b7798c5830676b34652052ff63209b3467861839
rajeshmn47 <rajeshmn47@gmail.com>
2022-02-23 23:43:11 +0530

    second commit

--- src/App.js
+++ src/App.js
@@ -3,23 +3,39 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <>
+    <div className='navbar'>
+  <div className='navbar-left'>
+  <p>RAJESH</p></div>
+  <div className='navbar-right'>
+<p>HOME</p>
+<p>ABOUT</p>
+<p>PROJECTS</p>
+<p>CONTACT</p>
+</div>
     </div>
-  );
+    <div className='about'><p>I AM RAJESH</p> 
+    <p>I AM A FULL STACK DEVELOPER</p>
+    </div>
+    <div className='center'>
+      <div className='leftskillstitle'>
+        <p>i am rajesh i worked with </p><p>following programming languages</p>
+      </div>
+      <div>
+    <div className='skillstitle'><h1>SKILLS</h1></div>
+    <div className='skills'>
+<div className='skill'>JAVASCRIPT</div>
+<div className='skill'>NODEJS</div>
+<div className='skill'>REACTJS</div>
+<div className='skill'>MONGODB</div>
+<div className='skill'>EXPRESSJS</div>
+<div className='skill'>JAVASCRIPT</div>
+    </div>
+    </div>
+    </div>
+    </>
+    
+  )
 }
 
 export default App;
