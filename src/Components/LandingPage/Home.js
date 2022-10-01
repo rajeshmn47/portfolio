f5feff4b1ffa1ac134619f27d4b7eb0b36bf32d0
tejasethinos <tejas.kesaria@ethinos.com>
2022-10-02 01:01:55 +0530

    work featured fix

--- src/Components/LandingPage/Home.js
+++ src/Components/LandingPage/Home.js
@@ -5,18 +5,27 @@ import Skills from "./AllSkills/Skills";
 import ContactUs from "./ContactUs/ContactUs";
 import projectData from "../AllProjects/projectsData.json";
 import WorkFeatured from "../Shared/WorkFeatured/WorkFeatured";
-
+import styled from '@emotion/styled'
 const Home = () => {
   document.title = "Welcome to Rajesh's Portfolio";
   const projects = projectData.slice(0, 6);
   return (
-    <div>
+    <Container>
   <HeaderMain/>
   <AboutMe/>
       <WorkFeatured projectData={projects}/>
       <ContactUs/>
-    </div>
+    </Container>
   );
 };
 
 export default Home;
+
+
+const Container= styled.div`
+@media only screen and (max-width: 600px) {
+.skills{
+  display:none;
+}
+overflow:hidden;
+}`
